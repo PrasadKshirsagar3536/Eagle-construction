4f44bdff8a88a22c5d2b17f6ca93ec4248339847
Prasad Kshirsagar <115228534+PrasadKshirsagar3536@users.noreply.github.com>
2024-03-11 18:48:14 +0530

    Delete src directory

--- src/main/java/com/ezio/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ezio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ezio.entity.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
